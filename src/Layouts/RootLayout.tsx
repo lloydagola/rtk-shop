c37818678c2472521d8b4db89511650fee57a5a4
lloydagola <lloydagola@gmail.com>
2023-12-24 15:50:36 +0300

    render component

--- src/Layouts/RootLayout.tsx
+++ src/Layouts/RootLayout.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { Heading } from "../components/Text";
+import { Outlet } from "react-router-dom";
 
 const RootLayout = () => {
-  return <div>RootLayout</div>;
+  return (
+    <>
+      <Heading variant="h2">RootLayout</Heading>
+
+      <main>
+        <Outlet />
+      </main>
+    </>
+  );
 };
 
 export default RootLayout;
