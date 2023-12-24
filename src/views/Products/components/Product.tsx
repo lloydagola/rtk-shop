d50d23568a8e8f3dffcf4393a3e656ff29723a03
lloydagola <lloydagola@gmail.com>
2023-12-24 18:43:34 +0300

    code cleanup

--- src/views/Products/components/Product.tsx
+++ src/views/Products/components/Product.tsx
@@ -4,6 +4,7 @@ import Card from "@mui/material/Card";
 import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
 import { IProduct } from "../../../common/types/types";
+import Box from "@mui/material/Box";
 
 interface IProductProps {
   product: IProduct;
@@ -11,34 +12,32 @@ interface IProductProps {
   removeFromCart?: (productId: number) => void;
 }
 
-const Product = ({ product, addToCart, removeFromCart }: IProductProps) => {
-  return (
-    <Grid item xs={12} lg={3}>
-      <Card sx={{ padding: "16px" }}>
-        <img src={product.image} alt="product" width="150px" />
-        <Typography variant="h5">{product.title}</Typography>
-        <Typography>KES: {product.price}</Typography>
-        <Typography>{product.description}</Typography>
-        {addToCart ? (
-          <Button
-            variant="contained"
-            sx={{ margin: "8px" }}
-            onClick={() => addToCart(product)}
-          >
-            Add to Cart
-          </Button>
-        ) : removeFromCart ? (
-          <Button
-            variant="contained"
-            sx={{ margin: "8px" }}
-            onClick={() => removeFromCart(product.id)}
-          >
-            Remove from Cart
-          </Button>
-        ) : null}
-      </Card>
-    </Grid>
-  );
-};
+const Product = ({ product, addToCart, removeFromCart }: IProductProps) => (
+  <Grid item xs={12} lg={3}>
+    <Card sx={{ padding: "16px" }}>
+      <img src={product.image} alt="product" width="150px" />
+      <Typography variant="h5">{product.title}</Typography>
+      <Typography>KES: {product.price}</Typography>
+      <Typography>{product.description}</Typography>
+      {addToCart ? (
+        <Button
+          variant="contained"
+          sx={{ margin: "8px" }}
+          onClick={() => addToCart(product)}
+        >
+          Add to Cart
+        </Button>
+      ) : removeFromCart ? (
+        <Button
+          variant="contained"
+          sx={{ margin: "8px" }}
+          onClick={() => removeFromCart(product.id)}
+        >
+          Remove from Cart
+        </Button>
+      ) : null}
+    </Card>
+  </Grid>
+);
 
 export default Product;
