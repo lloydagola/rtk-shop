417ca0bc477c7205830a48cf34e837ee5b6e43d3
lloydagola <lloydagola@gmail.com>
2023-12-24 17:36:30 +0300

    implemented redux logic

--- src/Layouts/RootLayout.tsx
+++ src/Layouts/RootLayout.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import { Provider } from "react-redux";
+
 import NavBar from "./NavBar";
+import store from "../state/store/store";
 
 const RootLayout = () => {
   return (
-    <>
+    <Provider store={store}>
       <NavBar />
       <main>
         <Outlet />
       </main>
-    </>
+    </Provider>
   );
 };
 
--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -6,6 +6,8 @@ import React, { useEffect, useState } from "react";
 
 import products from "../mocks/data/products";
 import { IProduct } from "../common/types/types";
+import { useDispatch } from "react-redux";
+import { add } from "../state/slices/cartSlice";
 
 const fetchProducts = async () => {
   //const result = await fetch("https://fakestoreapi.com/productss");
@@ -23,7 +25,10 @@ const Product = () => {
     })();
   }, []);
 
-  console.log({ products });
+  const dispatch = useDispatch();
+  const addToCart = (product: IProduct) => {
+    dispatch(add(product));
+  };
 
   const cards = products.map((product: IProduct) => (
     <Grid item xs={12} lg={3}>
@@ -32,7 +37,11 @@ const Product = () => {
         <Typography variant="h5">{product.title}</Typography>
         <Typography>KES: {product.price}</Typography>
         <Typography>{product.description}</Typography>
-        <Button variant="contained" sx={{ margin: "8px" }}>
+        <Button
+          variant="contained"
+          sx={{ margin: "8px" }}
+          onClick={() => addToCart(product)}
+        >
           Add to Cart
         </Button>
       </Card>
