3586b4e76a039a29f6067604a2b26fc42a232213
lloydagola <lloydagola@gmail.com>
2023-12-24 15:38:36 +0300

    added basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
 import React from "react";
-import logo from "./logo.svg";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+
 import "./App.css";
 import Product from "./components/Product";
+import Dashboard from "./components/Dashboard";
+import Cart from "./components/Cart";
 
 function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route>
+        <Route index element={<Dashboard />} />
+        <Route path="/cart" element={<Cart />} />
+      </Route>
+    )
+  );
   return (
     <div className="App">
-      <Product />
+      <RouterProvider router={router}>
     </div>
   );
 }
