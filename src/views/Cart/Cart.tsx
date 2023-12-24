52367eabd48f6cabebcbf177ea4a53020731b4b6
lloydagola <lloydagola@gmail.com>
2023-12-24 18:16:27 +0300

    implemented cart

--- src/views/Cart/Cart.tsx
+++ src/views/Cart/Cart.tsx
@@ -1,8 +1,34 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Grid from "@mui/material/Grid";
+
+import Product from "../Products/components/Product";
 import { Heading } from "../../common/components/Text";
+import { RootState } from "../../state/store/store";
+import { remove } from "../../state/slices/cartSlice";
 
 const Cart = () => {
-  return <Heading variant="h4">Cart</Heading>;
+  const cartList = useSelector((state: RootState) => state.cart);
+  const dispatch = useDispatch();
+
+  const removeFromCart = (id: number) => {
+    console.log("removing...");
+    dispatch(remove(id));
+  };
+  return (
+    <>
+      <section>
+        <Heading variant="h4">Cart</Heading>;
+      </section>
+      <section>
+        <Grid container spacing={3}>
+          {cartList.map((product) => (
+            <Product product={product} removeFromCart={removeFromCart} />
+          ))}
+        </Grid>
+      </section>
+    </>
+  );
 };
 
 export default Cart;
